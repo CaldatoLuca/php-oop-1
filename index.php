abcb5f99fe8b064c0b04060c7cc1deac001c519a
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-15 15:35:44 +0100

    feat: creazione delle due istanze

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once __DIR__ . '/models/movie.php';
+
+$tre_uomini_e_una_gamba = new Movie('Tre uomini e una gamba', 'Aldo, Giovanni e Giacomo', 1997, 'Commedia');
+$quo_vado = new Movie('Quo vado?', 'Checco Zalone', 2016, 'Commedia');
