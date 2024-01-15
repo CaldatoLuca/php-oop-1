e3c15b9e7c8120d59353d5b562ee4deac482e756
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-15 15:29:48 +0100

    first commit

---
+++ models/movie.php
@@ -0,0 +1,41 @@
+<?php
+
+class Movie
+{
+    public $title;
+    public $director;
+    public $year;
+    public $genre;
+    function __construct(
+        $_title,
+        $_director,
+        $_year,
+        $_genre
+    ) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+        $this->genre = $_genre;
+    }
+    /**
+     * setGenre: funzione per scrivere genere
+     *
+     * @param  string 
+     * @return void
+     */
+    public function setGenre($_genre)
+    {
+        $this->genre = $_genre;
+    }
+
+
+    /**
+     * getGenre: funzione per leggere genere movie
+     *
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+}
