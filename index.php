01dbf3743eb46994caa146a45632d5f186dcbdf0
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-15 15:39:38 +0100

    feat: stampa con echo dei valori e invocazione funzione(metodo)

--- index.php
+++ index.php
@@ -4,3 +4,24 @@ require_once __DIR__ . '/models/movie.php';
 
 $tre_uomini_e_una_gamba = new Movie('Tre uomini e una gamba', 'Aldo, Giovanni e Giacomo', 1997, 'Commedia');
 $quo_vado = new Movie('Quo vado?', 'Checco Zalone', 2016, 'Commedia');
+
+
+echo $tre_uomini_e_una_gamba->title;
+echo '<br>';
+echo $tre_uomini_e_una_gamba->director;
+echo '<br>';
+echo $tre_uomini_e_una_gamba->year;
+echo '<br>';
+echo $tre_uomini_e_una_gamba->getGenre();
+echo '<br>';
+
+echo '<hr />';
+
+echo $quo_vado->title;
+echo '<br>';
+echo $quo_vado->director;
+echo '<br>';
+echo $quo_vado->year;
+echo '<br>';
+echo $quo_vado->getGenre();
+echo '<br>';
